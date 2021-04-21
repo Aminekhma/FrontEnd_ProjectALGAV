dae08a84940b21c76a62494f5e2839e73581c9b9
Aminekhma <kheddramine@hotmail.fr>
2021-04-21 16:54:22 +0200

    part 3

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -9,25 +9,63 @@ import { TransactionService } from '../services/transaction.service';
 export class TransactionsComponent implements OnInit {
 
   listTransactions;
+  listCut;
+  date;
+  cpt;
 
   constructor(public transactionService : TransactionService) { 
     this.listTransactions = []
+    this.listCut = []
+
   }
 
   ngOnInit(): void {
     this.transactionService.getData().subscribe(res=>{
       this.listTransactions = res;
+
     },(err)=>{
       alert(err);
     })
   }
 
-  postTransactions(date,price,quantity,tigID,transaction_type){
-    this.transactionService.postTransaction({date,price,quantity,tigID,transaction_type}).subscribe(res=>{
+  refreshTransaction(){
+    this.transactionService.getData().subscribe(res => {
+      this.listTransactions = res;
+      this.listCut = []
+    },(err)=>{
+      alert('failerd loading json data');
+      console.log(err);
+    });
+  }
+
+  postTransactions(date,quantity,tigID,transaction_type){
+    this.transactionService.postTransaction({date,quantity,tigID,transaction_type}).subscribe(res=>{
       console.log(res);
+      this.refreshTransaction();
     },(err)=>{
       alert(err);
     })
+    this.refreshTransaction();
+
   }
+
+  getNewList(){
+    for(let p of this.listTransactions.reverse()){
+      if(this.listCut.length <= 100){
+        this.listCut.push(p)
+      }      
+    }
+    return this.listCut
+
+  }
+
+  convertDate(d){
+    this.date = new Date(d);
+    return this.date.getFullYear()+'-'+(this.date.getMonth()+1)+'-'+
+    this.date.getDate()+"  "+this.date.getHours()+':'+this.date.getMinutes();
+
+  }
+
+
 
 }
